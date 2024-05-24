dbed9357a5207aac1527cdb992597def3d322f41
Tiago Manuel De Almeida Manies <tiade-al@c1r11s1.42lisboa.com>
2024-05-24 14:25:00 +0100

    Noice

---
+++ get_next_line_bonus.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiade-al <tiade-al@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 13:52:50 by tiade-al          #+#    #+#             */
+/*   Updated: 2024/05/24 13:53:08 by tiade-al         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*str;
+
+	str = 0;
+	if (fd < 0 || fd > FOPEN_MAX || BUFFER_SIZE < 1)
+		return (0);
+	while (*buffer || read(fd, buffer, BUFFER_SIZE) > 0)
+	{
+		str = ft_strjoin(str, buffer);
+		if (ft_checknewline(buffer, 0))
+			break ;
+	}
+	return (str);
+}
